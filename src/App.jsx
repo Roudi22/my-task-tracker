f392d50317f97b241f4360d26e5bd0401e813bed
Roudi Alzkemi <roudi.alzkemi@gmail.com>
2021-12-08 15:05:50 +0100

    Add Header and Button components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
+import Header from "./components/Header";
+
+const initialTasks = [
+  {
+    id: 1,
+    text: "Tak 1",
+    day: "Dec 10th at 2:00 pm",
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: "Tak 2 ",
+    day: "Dec 12th at 5:00 pm",
+    reminder: false,
+  },
+  {
+    id: 3,
+    text: "Tak 3 ",
+    day: "Dec 13th at 9:00 am",
+    reminder: true,
+  },
+];
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="container">
+      <Header />
     </div>
   );
 }
