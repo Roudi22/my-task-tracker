fb288e0912583ec2fae87ed2e57df07b109fa08e
Roudi Alzkemi <roudi.alzkemi@gmail.com>
2021-12-08 15:50:26 +0100

    some other fixes

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -3,6 +3,11 @@ import PropTypes from "prop-types";
 
 
 function Tasks ({tasks}) {
+    if (!tasks || tasks.length === 0) {
+        return (
+            <p>No Tasks to display</p>
+        );
+    }
     return tasks.map((task) => (
         <p key={task.id}>{task.text}</p>
     ));
